700f080b727b4139d2ab276e028a7a53a573aa75
gokul-webzenith <gokul@webzenith.tech>
2026-02-24 16:26:16 +0530

    first commit

---
+++ my-app/apps/coolapp/app/_layout.tsx
@@ -0,0 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import "../global.css";
+
+const queryClient = new QueryClient();
+
+export default function RootLayout() {
+	return (
+		<SafeAreaProvider>
+			<QueryClientProvider client={queryClient}>
+				<Stack screenOptions={{ headerShown: false }}>
+					<Stack.Screen name="(tabs)" />
+				</Stack>
+			</QueryClientProvider>
+		</SafeAreaProvider>
+	);
+}
